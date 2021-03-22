64388ca02cfd8e9532d024804c3efaf067aa861e
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2021-03-22 11:23:02 +0100

    local tests

--- src/functions/newLolly.js
+++ src/functions/newLolly.js
@@ -1,19 +1,17 @@
-const faunadb = require('faunadb');
-const shortid = require('shortid');
-const querystring = require('querystring');
-const axios = require('axios');
+const faunadb = require("faunadb");
+const shortid = require("shortid");
+const querystring = require("querystring");
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const q = faunadb.query
+const q = faunadb.query;
 const client = new faunadb.Client({
-  secret: process.env.FAUNADB_SERVER_SECRET
-})
-
+  secret: process.env.FAUNADB_SERVER_SECRET,
+});
 
 /* export our lambda function as named "handler" export */
 exports.handler = (event, context, callback) => {
-
   // get the form data
   const data = querystring.parse(event.body);
 
@@ -21,23 +19,24 @@ exports.handler = (event, context, callback) => {
   const uniquePath = shortid.generate();
   data.lollyPath = uniquePath;
   const lolly = {
-    data: data
+    data: data,
   };
 
-
   // Create the lolly entry in the fauna db
-  client.query(q.Create(q.Ref('classes/lollies'), lolly))
+  client
+    .query(q.Create(q.Ref("classes/lollies"), lolly))
     .then((response) => {
-      console.log('success', response);
+      console.log("success", response);
 
       // Trigger a new build to freeze this lolly forever
-      axios.post('https://api.netlify.com/build_hooks/5d46fa20da4a1b70047f2f04')
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+      axios
+        .post(process.env.NETLIFY_BUILD_HOOK)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
 
       // Success! Go to a page to view the result
       return callback(null, {
@@ -45,15 +44,15 @@ exports.handler = (event, context, callback) => {
         statusCode: 302,
         headers: {
           Location: `/lolly/${uniquePath}`,
-        }
+        },
       });
-    }).catch((error) => {
-      console.error('error', error);
+    })
+    .catch((error) => {
+      console.error("error", error);
       // Error! return the error with statusCode 400
       return callback(null, {
         statusCode: 400,
-        body: JSON.stringify(error)
+        body: JSON.stringify(error),
       });
     });
-
-}
+};
